20e28e09610758ad86299ae84f6a2f18784caeef
shirohoo <shirohoo4102@gmail.com>
2021-10-21 15:17:25 +0900

    Rebuilding AbstractFullText

--- src/main/java/model/AbstractFullText.java
+++ src/main/java/model/AbstractFullText.java
@@ -1,43 +1,29 @@
 package model;
 
-import static java.util.Arrays.stream;
-import annotation.FullText;
 import java.util.Objects;
-import java.util.function.Predicate;
-import type.RecordType;
+import type.PaddingSymbol;
 
-public abstract class AbstractFullText<T extends AbstractFullText> {
+public abstract class AbstractFullText {
 
-    protected final Class<T> clazz;
+    protected final String paddingSymbol;
 
     protected final int rowSize;
 
-    public AbstractFullText(final Class<T> clazz, final int rowSize) {
-        this.clazz = clazz;
+    public AbstractFullText(final PaddingSymbol paddingSymbol, final int rowSize) {
+        Objects.requireNonNull(paddingSymbol, "padding symbol must not be null.");
+        if (rowSize < 0) {
+            throw new IllegalArgumentException("row size must be greater than 0.");
+        }
+        this.paddingSymbol = paddingSymbol.getSymbol();
         this.rowSize = rowSize;
     }
 
-    public int remainingSize(final int currentDataSize) {
-        final int remainingSize = this.rowSize - currentDataSize;
-        if (remainingSize < 0) {
-            throw new IllegalStateException(String.format(
-                "Unable to get remaining size. The entered size exceeds the total size. total size: %d. but, entered size: %d", rowSize, remainingSize
-            ));
-        }
-        return remainingSize;
+    public String getPaddingSymbol() {
+        return paddingSymbol;
     }
 
-    public int sizeOf(final RecordType type) {
-        return stream(clazz.getDeclaredFields())
-            .map(declaredField -> declaredField.getAnnotation(FullText.class))
-            .filter(Objects::nonNull)
-            .filter(equals(type))
-            .map(FullText::size)
-            .reduce(0, Integer::sum);
-    }
-
-    private Predicate<FullText> equals(final RecordType type) {
-        return annotation -> Objects.equals(annotation.recordType(), type);
+    public int getRowSize() {
+        return rowSize;
     }
 
 }
