c27c123cd4683cd385c9e28a8813f2ab189f6d63
shirohoo <shirohoo4102@gmail.com>
2021-10-22 20:23:07 +0900

    Write Javadoc in important class

--- src/main/java/fulltext/FullText.java
+++ src/main/java/fulltext/FullText.java
@@ -6,15 +6,28 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * {@link FullTextMapper}는 이 어노테이션을 참조하여 전문과 객체를 매핑합니다.
+ * <p>
+ * {@link FullTextMapper} references this annotation to map full text and objects.
+ */
 @Documented
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface FullText {
 
+    /**
+     * 전문에서 필드가 점유하는 길이를 정의합니다.
+     * <p>
+     * Defines the length occupied by the field in the full text.
+     */
     int length() default 0;
 
+    /**
+     * 해당 필드가 오로지 패딩만을 위한 필드인지를 나타냅니다.
+     * <p>
+     * Indicates whether the field is for padding only.
+     */
     boolean paddingOnly() default false;
-
-    Charset encoding() default Charset.UTF_8;
 
 }
--- src/main/java/fulltext/FullTextMapper.java
+++ src/main/java/fulltext/FullTextMapper.java
@@ -15,6 +15,13 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.logging.Logger;
 
+/**
+ * {@link FullTextMapper} 는 전문과 객체를 서로 매핑합니다.
+ * <p>
+ * 사용하기 위해서는 매핑하려는 Object에 @FullText가 알맞게 선언돼있어야 합니다.
+ * <p>
+ * FullTextMapper mapping full text and Object to each other. In order to use it, {@link FullText} must be properly declared in the object to be mapped.
+ */
 public final class FullTextMapper {
 
     private final static Logger log = Logger.getGlobal();
@@ -23,10 +30,14 @@ public final class FullTextMapper {
 
     /**
      * 이 정적 팩토리 메소드는 기본 생성자를 호출합니다.
+     * <p>
      * This static factory method is invoked default constructor.
      * <p>
+     * <p>
      * 반환된 인스턴스의 인코딩 유형은 기본적으로 UTF-8로 설정되어 있습니다.
+     * <p>
      * returned instance has encoding type set to UTF-8 by default.
+     * <p>
      *
      * @return instance of fulltext.FullTextMapper
      */
@@ -40,10 +51,14 @@ public final class FullTextMapper {
 
     /**
      * 이 팩토리 메서드는 fulltext.Charset을 전달받는 생성자를 호출합니다.
+     * <p>
      * This factory method calls the constructor taking fulltext.Charset as an argument.
      * <p>
+     * <p>
      * 기본 인코딩은 UTF-8이지만 다른 인코딩을 사용하려면 이 메서드를 호출해야 합니다.
+     * <p>
      * The default encoding is UTF-8, but you should invoke this method if you want to use a different encoding.
+     * <p>
      *
      * @return instance of fulltext.FullTextMapper
      */
@@ -57,21 +72,27 @@ public final class FullTextMapper {
 
     /**
      * 바이트 배열로 구성된 전문을 지정된 인코더로 인코딩 한 후, 전달받은 객체의 각 필드에 바인딩 합니다.
+     * <p>
      * After encoding the full text composed of a byte array with the specified encoder, it is binding to each field of the received object.
      * <p>
+     * <p>
      * 이 때 각 필드의 선언 타입에 따라 전문의 데이터를 형변환하여 바인딩합니다.
+     * <p>
      * At this time, the data of the full text is converted and binding to the declaration type of each field.
      * <p>
+     * <p>
      * 데이터 바인딩이 문제없이 완료되면 생성된 인스턴스를 반환합니다.
+     * <p>
      * If data binding completes without any problems, return the created instance.
      * <p>
+     * <p>
      * 몇가지 예외가 발생할 수 있지만, 예외가 발생 할 경우 로그를 남기고 항상 Optional을 반환합니다.
+     * <p>
      * Several exceptions may occur, but when an exception occurs, it logs and always returns Optional.
+     * <p>
      *
-     * @param bytes 전문에서 읽어온 바이트 배열 타입의 데이터입니다.
-     *              Byte array type data read from the full text.
-     * @param clazz 전문에서 읽어온 데이터를 바인딩하여 인스턴스화할 클래스입니다.
-     *              A class to instantiate by binding data read from the full text.
+     * @param bytes 전문에서 읽어온 바이트 배열 타입의 데이터입니다. Byte array type data read from the full text.
+     * @param clazz 전문에서 읽어온 데이터를 바인딩하여 인스턴스화할 클래스입니다. A class to instantiate by binding data read from the full text.
      * @return
      */
     public <T> Optional<T> readValue(final byte[] bytes, final Class<T> clazz) {
@@ -82,21 +103,27 @@ public final class FullTextMapper {
 
     /**
      * 전달받은 객체의 인스턴스를 생성하고, 전문을 읽어들여 생성한 인스턴스의 각 필드에 바인딩합니다.
+     * <p>
      * Creates an instance of the received object, reads the full text, and binds it to each field of the created instance.
      * <p>
+     * <p>
      * 이 때 각 필드의 선언 타입에 따라 전문의 데이터를 형변환하여 바인딩합니다.
+     * <p>
      * At this time, the data of the full text is converted and binding to the declaration type of each field.
      * <p>
+     * <p>
      * 데이터 바인딩이 문제없이 완료되면 생성된 인스턴스를 반환합니다.
+     * <p>
      * If data binding completes without any problems, return the created instance.
      * <p>
+     * <p>
      * 몇가지 예외가 발생할 수 있지만, 예외가 발생 할 경우 로그를 남기고 항상 Optional을 반환합니다.
+     * <p>
      * Several exceptions may occur, but when an exception occurs, it logs and always returns Optional.
+     * <p>
      *
-     * @param line  전문에서 읽어온 문자열 타입의 데이터입니다.
-     *              String type data read from the full text.
-     * @param clazz 전문에서 읽어온 데이터를 바인딩하여 인스턴스화할 클래스입니다.
-     *              A class to instantiate by binding data read from the full text.
+     * @param line  전문에서 읽어온 문자열 타입의 데이터입니다. String type data read from the full text.
+     * @param clazz 전문에서 읽어온 데이터를 바인딩하여 인스턴스화할 클래스입니다. A class to instantiate by binding data read from the full text.
      * @return
      */
     public <T> Optional<T> readValue(final String line, final Class<T> clazz) {
