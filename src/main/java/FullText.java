8990de3d7108b045abbd1c9d710ff9c1a911d100
shirohoo <shirohoo4102@gmail.com>
2021-10-22 11:09:06 +0900

    Add annotation @FullText

---
+++ src/main/java/FullText.java
@@ -0,0 +1,18 @@
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface FullText {
+
+    int value();
+
+    int length();
+
+    Charset encoding() default Charset.UTF_8;
+
+}
