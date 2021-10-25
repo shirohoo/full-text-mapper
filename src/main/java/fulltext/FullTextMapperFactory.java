608d070b1892830a27ad0a17eec69ea4eae7c348
shirohoo <shirohoo4102@gmail.com>
2021-10-25 14:42:52 +0900

    Add FullTextMapperFactory

---
+++ src/main/java/fulltext/FullTextMapperFactory.java
@@ -0,0 +1,18 @@
+package fulltext;
+
+public class FullTextMapperFactory {
+
+    private FullTextMapperFactory() {
+    }
+
+    public static FullTextMapper getLineFullTextMapper() {
+        return LineFullTextMapperHolder.instance;
+    }
+
+    private static class LineFullTextMapperHolder {
+
+        private static final FullTextMapper instance = LineFullTextMapper.create();
+
+    }
+
+}
