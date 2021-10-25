355d76628e66250bee0a3800d58e3d3147dbedca
shirohoo <shirohoo4102@gmail.com>
2021-10-25 14:59:13 +0900

    Add PaddingCharacter.java

---
+++ src/main/java/fulltext/PaddingCharacter.java
@@ -0,0 +1,32 @@
+package fulltext;
+
+import static java.util.Arrays.stream;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public enum PaddingCharacter {
+    SPACE(" "),
+    ZERO("0");
+
+    private static final Map<PaddingCharacter, String> MAP = stream(values())
+        .collect(Collectors.toUnmodifiableMap(Function.identity(), PaddingCharacter::getCharacter));
+
+    private final String character;
+
+    PaddingCharacter(final String character) {
+        this.character = character;
+    }
+
+    private String getCharacter() {
+        return character;
+    }
+
+    public static String findBy(final PaddingCharacter charset) {
+        if (!MAP.containsKey(charset)) {
+            throw new NoSuchElementException();
+        }
+        return MAP.get(charset);
+    }
+}
