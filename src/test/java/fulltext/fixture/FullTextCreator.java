c0d3ac69ec63d1ad8de021c73163962ffe53fc13
shirohoo <shirohoo4102@gmail.com>
2021-10-28 21:59:25 +0900

    Add ValidOptionModel.java

--- src/test/java/fulltext/fixture/FullTextCreator.java
+++ src/test/java/fulltext/fixture/FullTextCreator.java
@@ -8,6 +8,12 @@ public final class FullTextCreator {
             "3                                                                                                   ";
     }
 
+    public static String validOptionData() {
+        return "120211011                                                                                          " +
+            " 2siro      028                                                                                     " +
+            " 3                                                                                                   ";
+    }
+
     public static String validModel() {
         return "120211011                                                                                           "
             + "2      siro 28                                                                                "
