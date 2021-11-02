35f6aef63ab132e31b88f9caf2ee9d41c276eb10
shirohoo <shirohoo4102@gmail.com>
2021-11-02 20:05:48 +0900

    PadPosition 테스트를 정의했다

---
+++ src/test/java/fulltext/enums/PadPositionTest.java
@@ -0,0 +1,39 @@
+package fulltext.enums;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class PadPositionTest {
+
+    @MethodSource
+    @ParameterizedTest
+    void isNone(final PadPosition PadPosition, final boolean expected) throws Exception {
+        assertThat(PadPosition.isNone()).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> isNone() {
+        return Stream.of(
+            Arguments.of(PadPosition.NONE, true),
+            Arguments.of(PadPosition.LEFT_PAD, false),
+            Arguments.of(PadPosition.RIGHT_PAD, false)
+        );
+    }
+
+    @MethodSource
+    @ParameterizedTest
+    void isLeft(final PadPosition PadPosition, final boolean expected) throws Exception {
+        assertThat(PadPosition.isLeft()).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> isLeft() {
+        return Stream.of(
+            Arguments.of(PadPosition.NONE, false),
+            Arguments.of(PadPosition.LEFT_PAD, true),
+            Arguments.of(PadPosition.RIGHT_PAD, false)
+        );
+    }
+
+}
